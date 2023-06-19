81e4be543eccf63364501bc82ea9a8f9bbcf4fa7
EnkiW <ytcahekyttpocto@gmail.com>
2023-06-19 15:37:21 +0300

    fin

---
+++ script.js
@@ -0,0 +1,14 @@
+function Sum() {
+    let sum = 0;
+
+    return function(num) {
+        sum += num;
+        return sum;
+    };
+}
+
+const sum = Sum();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
